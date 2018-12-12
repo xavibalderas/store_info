1e1a74c13b5bc9a69702058acc5ff981fcb83c91
Francisco Javier Balderas Cejudo <frbal5@retchso-xw0007.ikea.com>
2018-12-12 14:53:30 +0100

    First commit

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.get('/en', (req, res) => res.render('pages/english'));
+app.get('/de', (req, res) => res.render('pages/german'));
---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+
+
+function slide(position){
+  $('.slide').css('display', 'none');
+  $('#' + position).css('display', 'block');
+}
